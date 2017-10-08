0253b286dfa042420639ba54508b6b4b620769ee
Khoa Ho <hokhoa@grinnell.edu>
2017-10-08 01:04:38 -0500

    update imports

--- src/edu/grinnell/sortingvisualizer/SortingVisualizer.java
+++ src/edu/grinnell/sortingvisualizer/SortingVisualizer.java
@@ -4,19 +4,23 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
+import edu.grinnell.sortingvisualizer.audio.NoteIndices;
+import edu.grinnell.sortingvisualizer.rendering.ArrayPanel;
+import edu.grinnell.sortingvisualizer.rendering.ControlPanel;
+
 public class SortingVisualizer {
 
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
 
-    public static void main(String[] args) throws InterruptedException {        
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new BorderLayout());
         NoteIndices notes = new NoteIndices(0);
-        
-        ArrayPanel   arrayPanel   = new ArrayPanel(notes, WIDTH, HEIGHT);
-        ControlPanel controlPanel = new ControlPanel(notes, arrayPanel); 
-        
+
+        ArrayPanel arrayPanel = new ArrayPanel(notes, WIDTH, HEIGHT);
+        ControlPanel controlPanel = new ControlPanel(notes, arrayPanel);
+
         frame.setTitle("Sorting Visualizer");
         frame.add(controlPanel, BorderLayout.PAGE_END);
         frame.add(arrayPanel, BorderLayout.CENTER);
