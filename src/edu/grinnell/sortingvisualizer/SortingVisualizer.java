dee1810f0b5dfee31b57979789cd1c38932c141d
HongyuanZhang <zhanghon@grinnell.edu>
2017-10-11 12:20:26 -0500

    Add files via upload

--- src/edu/grinnell/sortingvisualizer/SortingVisualizer.java
+++ src/edu/grinnell/sortingvisualizer/SortingVisualizer.java
@@ -8,11 +8,25 @@ import edu.grinnell.sortingvisualizer.audio.NoteIndices;
 import edu.grinnell.sortingvisualizer.rendering.ArrayPanel;
 import edu.grinnell.sortingvisualizer.rendering.ControlPanel;
 
+/***
+ * CSC207 The Sounds of Sorting
+ *
+ * This class is a driver for both a visualizer and audibilizer for the various sorting algorithms.
+ * 
+ * @author zhanghon hokhoa
+ *
+ */
+
 public class SortingVisualizer {
 
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
 
+    /***
+     * This is the main driver of the Sorting Visualizer program.
+     * @param args, command-line arguments (not needed in our case)
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new BorderLayout());
